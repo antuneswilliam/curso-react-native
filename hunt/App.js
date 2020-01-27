6e3a38735e4d7cd17994d37cc5ccef582fd909a3
wantunes <wantunes@stara.com.br>
2020-01-27 16:40:52 -0300

    init

---
+++ hunt/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Appl from './src';
+
+export default function App() {
+  return (
+      <Appl />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#999',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
