1dbbbbf794460c7a2679f772189c9445006beb6b
wantunes <wantunes@stara.com.br>
2020-01-27 17:00:58 -0300

    fix

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # curso-react-native
+
+expo init hunt
+
+yarn add react-navigation
+
+yarn add react-native-gesture-handler
+
+yarn add react-navigation-stack
+
+yarn add react-native-safe-area-context
+
+expo start --web
--- hunt/App.js
+++ hunt/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Appl from './src';
+// import React from 'react';
+// import { StyleSheet, View, Text } from 'react-native';
+// import Appl from './src';
 
-export default function App() {
-  return (
-      <Appl />
-  );
-}
+// export default function App() {
+//   return (
+//       <Appl />
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#999',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#999',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+import { createAppContainer } from 'react-navigation';
+const App = createAppContainer(Appl);
+
+export default App;
